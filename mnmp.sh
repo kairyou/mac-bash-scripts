3302f588726dfeb22b1b7a7b0e75714d265672f1
Leon.Xu <xhl@live.it>
2015-07-08 14:29:00 +0800

    fix: sometimes stop error

--- mnmp.sh
+++ mnmp.sh
@@ -14,6 +14,8 @@ start()
 {
     npids=`ps aux | grep -i nginx | grep -v grep | awk '{print $2}'`
     if [ ! -n "$npids" ]; then
+        tmp='/usr/local/var/run/nginx/fastcgi_temp'
+        [ -d "$tmp" ] && sudo chmod -R 755 $tmp
         echo "starting php-fpm ..." && $PHPFPM start
         # unable to bind listening socket for address '127.0.0.1:xx': Address already in use # killall -c php-fpm
 
@@ -32,15 +34,18 @@ stop()
     # fi
 
     echo "stopping mnmp ..."
-    $PHPFPM stop # killall -c php-fpm
-    sudo $NGINX -s stop
-    $MYSQL stop # killall -c mysqld
-
+    # $PHPFPM stop
+    killall -c php-fpm
+    # sudo $NGINX -s stop
+    sudo killall -c nginx
+    # $MYSQL stop
+    killall -c mysqld
 }
 # config()
     # nginx -V # /usr/local/etc/nginx/nginx.conf
     # mysql –verbose –help | grep -A 1 'Default options' # /usr/local/opt/mysql/my.cnf
-    # /usr/local/opt/php56/sbin/php-fpm -i | grep 'Loaded Configuration File' # /usr/local/etc/php/5.6/php.ini, /usr/local/etc/php/5.6/php-fpm.conf
+    # /usr/local/opt/php56/sbin/php-fpm -i | grep 'Loaded Configuration File'
+    # /usr/local/etc/php/5.6/php.ini, /usr/local/etc/php/5.6/php-fpm.conf, /tmp/php-fpm.log
 case $param in
     'start')
         start;;
